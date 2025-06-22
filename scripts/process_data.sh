9f2fb021d260cfb6dbaacc82ba3531d685f17535
janiopi <janio072003@hotmail.com>
2025-06-22 12:38:14 -0500

    feat[#3]:Agregar script process_data.sh procesa todos los cambios dentro de app/data

---
+++ scripts/process_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+echo "DEBUG: Script inicializa con args: $@" >> /app/reports/process_data_debug.log
+if [ $# -ne 1 ]; then
+    echo "ERROR: Se requiere exactamente un solo argumento" >> /app/reports/process_data_debug.log
+    exit 1
+fi
+FILE="$1"
+echo "DEBUG: Procesando : $FILE" >> /app/reports/process_data_debug.log
+if [ ! -f "$FILE" ]; then
+    echo "ERROR:  $FILE no existe" >> /app/reports/process_data_debug.log
+    exit 1
+fi
+echo "$(date -u +'%Y-%m-%d %H:%M:%S') - Processando : $FILE" >> /app/reports/processed_files.log
+cat "$FILE" >> /app/reports/processed_files.log
+echo "DEBUG: Proceso exitoso " >> /app/reports/process_data_debug.log
